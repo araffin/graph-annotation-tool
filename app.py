6e8fc6e1ee0f3c03ecb377ef529168fbf65d594f
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-03-21 14:45:04 +0100

    Save labels to json file

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division
 import json
 import os
 import numpy as np
@@ -10,27 +11,39 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 port = 5000
 
-images = ['demo.jpg', 'example.jpg']
+DATASET_NAME = "ironcar_train"
+DATASET_FOLDER = "static/img/ironcar_train/"
+
+images = [f for f in os.listdir(DATASET_FOLDER) if f.endswith('.jpg')]
+images.sort(key=lambda name: int(name.split('.jpg')[0]))
+images_path = [DATASET_FOLDER + im for im in images]
 labels = {}
+try:
+    with open('data/{}_labels.json'.format(DATASET_NAME), 'r') as f:
+        labels = json.load(f)
+except IOError:
+    pass
 
 @app.route("/", methods=["GET"])
 def home():
     idx = int(request.args.get('idx', 0))
     idx = np.clip(idx, 0, len(images) - 1)
-    image = images[idx]
+    image = images_path[idx]
     percent = round(100 * ((idx + 1) / len(images)), 0)
-    return render_template('home.html', idx=idx, total=len(images), image=images[idx], percent=percent)
+    label = labels.get(images[idx], None)
+    if label is None:
+        # get previous image label
+        label = labels.get(images[idx - 1], [])
+    return render_template('home.html', idx=idx, total=len(images), image=image, percent=percent, label=label)
 
 @app.route("/save_labels", methods=["POST"])
 def save_labels():
-    new_labels = json.loads(request.data.decode('utf8'))
-    idx = request.form.get('idx')
-    print(new_labels)
+    label = json.loads(request.data.decode('utf8'))
     global labels
-
-    # with open('data/skills_remove_list.json', 'w') as f:
-    #     json.dump(labels, f)
-    return jsonify(status="ok")
+    labels[images[int(label['idx'])]] = label['values']
+    with open('data/{}_labels.json'.format(DATASET_NAME), 'w') as f:
+        json.dump(labels, f)
+    return jsonify(status="ok", label=label)
 
 if __name__ == '__main__':
     app.run(port=port)
